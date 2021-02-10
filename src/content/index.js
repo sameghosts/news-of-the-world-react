1d17465d7425cb6ab504d16d8f1bcaeeb4641625
sameghosts <seanfeiner.brown@gmail.com>
2021-02-10 13:45:33 -0800

    added state, have api running refactored for query state storage

--- src/content/index.js
+++ src/content/index.js
@@ -6,10 +6,27 @@ import { Landing } from './Landing';
 
 import '../styling/style.css';
 
+console.log(process.env.REACT_APP_API_KEY)
+
 // export const is quicker but must be imported directly
 // rather than as a default
 // refer to index.js at src root to see how import works
 export const App = () => {
+    //state for query, useful for when user is searching
+    const [query, setQuery] = useState(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`)
+    // establish state for storing of the news 
+    const [newsData, setNewsData] = useState([]);
+    // Use effect hook for fetch API call from News API 
+    useEffect(() => {
+        fetch(query)
+        .then(response => response.json())
+        .then(jsonData => {
+            console.log(jsonData.articles);
+            if (jsonData.articles){
+                setNewsData(jsonData.articles)
+            }
+        })
+    }, [])
     return (
         // Router for setting routes
         <Router>
